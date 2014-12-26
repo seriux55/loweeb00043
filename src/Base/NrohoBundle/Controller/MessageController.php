ce670849316a3326861873e7392a8d0a840177b5
seriux55 <seriux55@hotmail.com>
2014-12-26 22:03:22 +0100

    Quelques modifs
    quelques modifications

--- src/Base/NrohoBundle/Controller/MessageController.php
+++ src/Base/NrohoBundle/Controller/MessageController.php
@@ -11,6 +11,16 @@ class MessageController extends Controller
     public function messageAction()
     {     
         $idc = $this->get('security.context')->getToken()->getUser()->getId();
+        $message = $this->getDoctrine()->getRepository('BaseNrohoBundle:Message')
+                        ->createQueryBuilder('a')
+                        ->addSelect('b')->leftJoin('a.user', 'b')
+                        ->addSelect('c')->leftJoin('a.product', 'c')
+                        ->addSelect('d')->leftJoin('a.userDist', 'd')
+                        ->where('((a.user = :idc AND a.userDist = :id) OR (a.user = :id AND a.userDist = :idc)) AND a.product = :product')
+                        ->setParameter('idc', $idc)->setParameter('id', '29969')->setParameter('product', '857')
+                        ->orderBy('a.id','ASC')
+                        ->setMaxResults(7)
+                        ->getQuery()->getResult();
         $db  = $this->get('database_connection');
         $row = $db->prepare(
                             "SELECT u1.id AS u1_id, u1.secondename AS u1_secondename, u1.gender AS u1_gender, u2.id AS u2_id, u2.secondename AS u2_secondename, u2.gender AS u2_gender, Message.depot, Message.user_id AS M_user_id, Product.user_id AS P_user_id, Message.userDist_id, Product.id AS P_id
@@ -32,13 +42,12 @@ class MessageController extends Controller
         $message = array();
         while ($data = $row->fetch()) {
             if(!in_array($data['M_user_id'],$d)){
-                //if($data['u1_id'] == $idc) $id = $data['u2_id']; else $id = $data['u1_id'];
 		$message[] = array(
-                    'user_id'       => $data['M_user_id'], //$data['user_id'],
+                    'user_id'       => $data['M_user_id'],
                     'product_id'    => $data['P_id'],
                     'gender'        => $data['u1_gender'],
                     'secondename'   => $data['u1_secondename'],
-                    'depot'         => $data['depot'],
+                    'depot'         => $data['depot']
 		);
 		$d[] = $data['M_user_id'];
             }
@@ -52,17 +61,15 @@ class MessageController extends Controller
     {
         $idc = $this->get('security.context')->getToken()->getUser()->getId();
         $message = $this->getDoctrine()->getRepository('BaseNrohoBundle:Message')
-                   ->createQueryBuilder('a')
-                   ->addSelect('b')->leftJoin('a.user', 'b')
-                   ->addSelect('c')->leftJoin('a.product', 'c')
-                   ->addSelect('d')->leftJoin('a.userDist', 'd')
-                   ->where('((a.user = :idc AND a.userDist = :id) OR (a.user = :id AND a.userDist = :idc)) AND a.product = :product')
-                   ->setParameter('idc', $idc)->setParameter('id', $id)->setParameter('product', $product)
-                   ->orderBy('a.id','ASC')
-                   ->setMaxResults(7)
-                   ->getQuery()->getResult();
-        //$message = $qb->getQuery()->getResult();
-        
+                        ->createQueryBuilder('a')
+                        ->addSelect('b')->leftJoin('a.user', 'b')
+                        ->addSelect('c')->leftJoin('a.product', 'c')
+                        ->addSelect('d')->leftJoin('a.userDist', 'd')
+                        ->where('((a.user = :idc AND a.userDist = :id) OR (a.user = :id AND a.userDist = :idc)) AND a.product = :product')
+                        ->setParameter('idc', $idc)->setParameter('id', $id)->setParameter('product', $product)
+                        ->orderBy('a.id','ASC')
+                        ->setMaxResults(7)
+                        ->getQuery()->getResult();
         $serializer = $this->container->get('serializer');
         $reports = $serializer->serialize($message, 'json');
         return new Response($reports);
