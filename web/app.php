a6e85422d9879823ee73b8c1427b19bd3449238e
seriux55 <seriux55@hotmail.com>
2014-12-26 23:55:11 +0100

    Quelques modifs
    quelques modifications

--- web/app.php
+++ web/app.php
@@ -1,6 +1,5 @@
 <?php
 
-use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
@@ -14,7 +13,6 @@ $loader->register(true);
 */
 
 require_once __DIR__.'/../app/appKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
