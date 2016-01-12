cfda9c3e02399e3a7f55523dd4280c1d82d9aefd
seriux55 <seriux55@hotmail.com>
2016-01-12 23:07:21 +0100

    Ajout de sitemap

--- src/Base/NrohoBundle/Controller/SitemapsController.php
+++ src/Base/NrohoBundle/Controller/SitemapsController.php
@@ -3,39 +3,31 @@
 namespace Base\NrohoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class SitemapsController extends Controller
 {
-    /**
-     * @Route("/sitemap.{_format}", name="sample_sitemaps_sitemap", Requirements={"_format" = "xml"})
-     * @Template("BaseNrohoBundle:Sitemaps:sitemap.xml.twig")
-     */
-    public function sitemapAction() 
+    public function sitemapAction()
     {
-        $em = $this->getDoctrine();
-        
         $urls = array();
         $hostname = $this->getRequest()->getHost();
 
         // add some urls homepage
         $urls[] = array('loc' => $this->get('router')->generate('nroho_base_default'), 'changefreq' => 'monthly', 'priority' => '1.0');
 
-        /*
-        // multi-lang pages
-        foreach($languages as $lang) {
-            $urls[] = array('loc' => $this->get('router')->generate('home_contact', array('_locale' => $lang)), 'changefreq' => 'monthly', 'priority' => '0.3');
-        }
+        $urls[] = array('loc' => $this->get('router')->generate('nroho_base_add'), 'changefreq' => 'monthly', 'priority' => '1.0');
+
+        $urls[] = array('loc' => $this->get('router')->generate('fos_user_security_login'), 'changefreq' => 'monthly', 'priority' => '1.0');
         
-        // urls from database
-        $urls[] = array('loc' => $this->get('router')->generate('nroho_base_produit', array('_locale' => 'fr')), 'changefreq' => 'weekly', 'priority' => '0.7');
-        */
+        // detail du covoiturage
+        $products = $this->getDoctrine()->getRepository('BaseNrohoBundle:Product')
+            ->createQueryBuilder('a')
+            ->where("a.valid = '1'")
+            ->getQuery()->getResult();
         
         // service
-        foreach ($em->getRepository('BaseNrohoBundle:Product')->findAll() as $product) {
+        foreach ($products as $product) {
             $urls[] = array('loc' => $this->get('router')->generate('nroho_base_produit', 
-                    array('id' => $product->getId())), 'changefreq' => 'weekly','priority' => '0.5');
+                    array('id' => $product->getId())), 'changefreq' => 'weekly', 'priority' => '0.5');
         }
 
         return array('urls' => $urls, 'hostname' => $hostname);
